3cbba653f567547652c4c07ab938340704ef1c89
zephyr-f <79936180+zephyr-f@users.noreply.github.com>
2022-04-29 00:06:08 +0300

    parse prod values

--- nutriApp.js
+++ nutriApp.js
@@ -1,17 +1,22 @@
 const userInput = prompt('שם מוצר לחיפוש');
-let ansPlace = document.getElementById('resCont');
+const ansPlace = document.getElementById('resCont');
 const noRes = '<h3>לא נמצאו תוצאות</h3>';
-const getNutri = async () => {
+let nuTable = document.getElementById('nuTableCont');
+function replacer(key, value) {
+  // Filtering out empty properties
+  if (value) return value;
+}
+async function getNutri() {
   try {
     const response = await axios.get(
       `https://data.gov.il/api/3/action/datastore_search?resource_id=c3cb0630-0650-46c1-a068-82d575c094b2&q=${userInput}`
     );
     console.log(response);
-    const answer = response.data.result.records
+    recsPath = response.data.result.records;
+    answer = recsPath
       .map(
-        x =>
-          `<div class="resItemFrame">
-          <p class="res" id=${x.smlmitzrach}>
+        (x, y) => `<div class="resItemFrame">
+          <p class="res" id=${x.smlmitzrach} data-index=${y} >
           ${x.shmmitzrach}
           </p>
           </div>`
@@ -26,10 +31,27 @@ const getNutri = async () => {
     } else {
       // success
       ansPlace.innerHTML = answer;
+      listen();
     }
   } catch (error) {
     console.log(error);
     ansPlace.innerHTML = noRes;
   }
-};
+}
 getNutri();
+
+function nuTableDisp() {
+  console.log(this.dataset.index);
+  let recStr = JSON.stringify(recsPath[this.dataset.index], replacer, '\t');
+  nuTable.innerHTML = `<p>${recStr}</p>`;
+  console.log(`<p>${recStr}</p>`);
+  // document.getElementById('nuTableCont').innerHTML = '<p>test</p>';
+  // // need to pass id to listener https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#getting_data_into_and_out_of_an_event_listener
+}
+listen = async () => {
+  const resItems = document.getElementsByClassName('res');
+  for (let i = 0; i < resItems.length; i++) {
+    // console.log(this);
+    resItems[i].addEventListener('click', nuTableDisp);
+  }
+};
