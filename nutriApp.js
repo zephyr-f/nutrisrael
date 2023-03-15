fd7f15d886dbab278318594577a014617551514b
Tzafrir Forsher <zforsher@gmail.com>
2023-03-15 14:47:46 +0200

    register sw.js

--- nutriApp.js
+++ nutriApp.js
@@ -1,3 +1,17 @@
+if ('serviceWorker' in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register('/sw.js').then(
+    registration => {
+      console.log('Service worker registration succeeded:', registration);
+    },
+    error => {
+      console.error(`Service worker registration failed: ${error}`);
+    }
+  );
+} else {
+  console.error('Service workers are not supported.');
+}
 const userInput = prompt('שם מוצר לחיפוש');
 const ansPlace = document.getElementById('resCont');
 const noRes = '<h3>לא נמצאו תוצאות</h3>';
